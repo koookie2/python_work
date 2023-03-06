75d8380a12fc1dff4076d89f3a4bc8bdf178c4da
koookie2 <kavinjgodwin@gmail.com>
2023-03-06 18:20:03 -0500

    Python Crash Course

--- Chapter 6/ex_6-3_glossary.py
+++ Chapter 6/ex_6-3_glossary.py
@@ -1,2 +1,6 @@
-glossary = {}
+glossary = {.append(): 'adds an element to a list',
+            'insert': 'inserts an element into a certain location in a list',
+            'del'   deletes an element from a list
+# LIST.pop()   deletes an element from a list, but keeps that element stored as a variable
+# LIST.remove()   removes a value from a list}
 print(glossary)
