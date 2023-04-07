9b659d86046fc5bdf334f8f7a4f24b7b22f714bf
koookie2 <kavinjgodwin@gmail.com>
2023-04-07 10:41:58 -0400

    Chapter_10 commit 1

--- Chapter_10/main_10_1_file_reader.py
+++ Chapter_10/main_10_1_file_reader.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
-path = Path('chapter 10\pi_digits.txt')
+path = Path('chapter_10/pi_digits.txt')
 contents = path.read_text()
-print(contents)
+
+lines = contents.splitlines()
+for line in lines:
+    print(line)
