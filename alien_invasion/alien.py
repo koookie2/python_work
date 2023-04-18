ae222a641fc17d8d92ee7679360fe409c7b816eb
koookie2 <kavinjgodwin@gmail.com>
2023-04-17 23:23:57 -0400

    chapter_13_commit_2

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -19,21 +19,3 @@ class Alien(Sprite):
 
         # Store a float for the ship's exact horizontal position.
         self.x = float(self.rect.x)
-
-    def update(self):
-        """Update the ships's position based on movement flags."""
-        # The temporary attribute "x" stores where the ship should actually be,
-        # since the ship will only take the integer part.
-        
-        # Update the ship's x value, not the rect.
-        if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.x += self.settings.ship_speed
-        if self.moving_left and self.rect.left > 0: # can be replaced with "self.screen_rect.left"
-            self.x -= self.settings.ship_speed
-        
-        # Update rect object from self x.
-        self.rect.x = self.x
-    
-    def blitme(self):
-        """Draw the ship at its current location."""
-        self.screen.blit(self.image, self.rect)
