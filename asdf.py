61f24b05e7933b66a7431801d8c7bc8be27756f8
koookie2 <kavinjgodwin@gmail.com>
2023-05-25 08:38:05 -0400

    chapter_18_commit_2

---
+++ asdf.py
@@ -0,0 +1,8 @@
+class Asdf:
+    test = "asdf"
+
+    def __str__(self) -> str:
+        return "hghghj"
+
+a = Asdf()
+print(str(a))
--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -5,6 +5,23 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Return a string representation of the model."""
         return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField(default=None)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self) -> str:
+        """Return a simple string representing the entry."""
+        if self.text < 51:
+            return self.text
+            # return f"{self.topic}: {self.text}
+        else:
+            return f"{self.text[:50]}..."
